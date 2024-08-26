f28ebb466b10e09cbfeaba4fdb147b68c4717718
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-08-26 23:37:07 +0530

    Fix: edit style for main wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-14">
      <h1 className="text-9xl font-serif font-extrabold ">Hello, <span className="bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent">Ronit</span></h1>
+     <Button>Sign in</Button>
     </main>
   );
 }

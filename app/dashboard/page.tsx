26a24709e4de9cbbe636054e0e726d2e9f0ff075
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-08-26 23:56:37 +0530

    Add: setup Kinde auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardIndexPage(){
+    return(
+        <h1>User Dashboard</h1>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-14">
      <h1 className="text-9xl font-serif font-extrabold ">Hello, <span className="bg-gradient-to-r from-indigo-400 to-cyan-400 bg-clip-text text-transparent">Ronit</span></h1>
-     <Button>Sign in</Button>
+     <RegisterLink>Sign up</RegisterLink>
     </main>
   );
 }

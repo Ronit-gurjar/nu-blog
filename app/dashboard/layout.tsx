7cda07f5968eac294612b7136c28da5c13150bc4
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-09-11 18:25:32 +0530

    Chore: remove style from header

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -41,7 +41,7 @@ export default function DashboardLayout({children} : {children : ReactNode}){
             </div>
 
             <div className="flex flex-col">
-                <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+                <header className="flex h-14 items-center gap-4 px-4 lg:h-[60px] lg:px-6">
                     <div className="ml-auto flex items-center gap-x-5">
                     <ThemeToggle/>
                     </div>

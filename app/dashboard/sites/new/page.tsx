1664a5c151bdb060e2887032a08b1c3dcbce0940
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-09-15 01:41:23 +0530

    Add: validation and action to form for site creation

--- app/dashboard/sites/new/page.tsx
+++ app/dashboard/sites/new/page.tsx
@@ -1,10 +1,32 @@
+"use client"
+
+import { CreateSiteAction } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useFormState } from "react-dom";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { siteSchema } from "@/lib/zodSchema";
 
 export default function NewSiteRoute(){
+
+    const [lastResult, action] = useFormState(CreateSiteAction, undefined)
+    const [form , fields] = useForm({
+        lastResult,
+
+        onValidate({formData}){
+            return parseWithZod(formData,{
+                schema: siteSchema,
+            });
+        },
+
+        shouldValidate: "onBlur",
+        shouldRevalidate: "onInput",
+    });
+
     return(
     <div className="flex flex-col flex-1 items-center justify-center">
         <Card>
@@ -14,25 +36,43 @@ export default function NewSiteRoute(){
                 Create your Site here. Click the button below once your done...
                 </CardDescription>
             </CardHeader>
-            <CardContent>
-            <div className="flex flex-col gap-y-6">
-                <div className="grid gap-2">
-                    <Label>Site</Label>
-                    <Input placeholder="Site Name" />
+            <form id={form.id} onSubmit={form.onSubmit} action={action}>
+                <CardContent>
+                <div className="flex flex-col gap-y-6">
+                    <div className="grid gap-2">
+                        <Label>Site</Label>
+                        <Input 
+                        name={fields.name.name} 
+                        key={fields.name.key} 
+                        defaultValue={fields.name.initialValue} 
+                        placeholder="Site Name" />
+                        <p className="text-red-500 text-sm">{fields.name.errors}</p>
+                    </div>
+ 
+                    <div className="grid gap-2">
+                        <Label>Subdirectory</Label>
+                        <Input
+                        name={fields.subdirectory.name} 
+                        key={fields.subdirectory.key} 
+                        defaultValue={fields.subdirectory.initialValue} 
+                        placeholder="Subdirectory Name" />
+                        <p className="text-red-500 text-sm">{fields.subdirectory.errors}</p>
+                    </div>
+                    <div className="grid gap-2">
+                        <Label>Description</Label>
+                        <Textarea 
+                        name={fields.description.name} 
+                        key={fields.description.key} 
+                        defaultValue={fields.description.initialValue} 
+                        placeholder="A Description for your site" />
+                        <p className="text-red-500 text-sm">{fields.description.errors}</p>
+                    </div>     
                 </div>
-                <div className="grid gap-2">
-                    <Label>Subdirectory</Label>
-                    <Input placeholder="Subdirectory Name" />
-                </div>
-                <div className="grid gap-2">
-                    <Label>Description</Label>
-                    <Textarea placeholder="A Description for your site" />
-                </div>     
-            </div>
-            </CardContent>
-            <CardFooter>
-                <Button>Create</Button>
-            </CardFooter>
+                </CardContent>
+                <CardFooter>
+                    <Button>Create</Button>
+                </CardFooter>
+            </form>
         </Card>
     </div>
     )

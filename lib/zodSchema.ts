ef55d51244613156a89f2d5e6638246cb61321f3
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-09-17 02:25:39 +0530

    Create: schema for post

--- lib/zodSchema.ts
+++ lib/zodSchema.ts
@@ -5,3 +5,11 @@ export const siteSchema = z.object({
     subdirectory: z.string().min(3).max(35),
     description: z.string().min(3).max(150),
 });
+
+export const PostSchema = z.object({
+    title: z.string() .min (1).max(100),
+    slug: z.string(). min (1) .max (190),
+    coverImage: z.string() .min(1),
+    smallDescription: z.string() .min(1).max(200),
+    articleContent: z.string() .min(1),
+});

08b1d9836b08aa8af655dc6a974049f044b3dc0c
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-09-19 01:36:10 +0530

    Create: table component to show posts/articles

---
+++ app/components/sites/BlogTable.tsx
@@ -0,0 +1,129 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+import { MoreHorizontal } from "lucide-react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+interface Post {
+    id: string;
+    title: string;
+    image: string;
+    createdAt: Date;
+  }
+  
+  interface PostDataProps {
+    posts: Post[];
+    siteId : string
+  }
+  
+export default function BlogTable({ posts, siteId }: PostDataProps) {
+    return (
+        <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Articles</CardTitle>
+            <CardDescription>
+              Manage your Articles in a simple and intuitive interface
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Image</TableHead>
+                  <TableHead>Title</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Created At</TableHead>
+                  <TableHead className="text-right">Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {posts.map((item) => (
+                  <TableRow key={item.id}>
+                    <TableCell>
+                      <Image
+                        src={item.image}
+                        width={64}
+                        height={64}
+                        alt="Article Cover Image"
+                        className="size-16 rounded-md object-cover"
+                      />
+                    </TableCell>
+                    <TableCell className="font-medium">
+                      {item.title}
+                    </TableCell>
+                    <TableCell>
+                      <Badge
+                        variant="outline"
+                        className="bg-green-500/10 text-green-500"
+                      >
+                        Published
+                      </Badge>
+                    </TableCell>
+                    <TableCell>
+                    {new Date(item.createdAt).toLocaleDateString()}
+                    </TableCell>
+
+                    <TableCell className="text-end">
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button size="icon" variant="ghost">
+                            <MoreHorizontal className="size-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem asChild>
+                            <Link
+                              href={`/dashboard/sites/${siteId}/${item.id}`}
+                            >
+                              Edit
+                            </Link>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem asChild>
+                            <Link
+                              href={`/dashboard/sites/${siteId}/${item.id}/delete`}
+                            >
+                              Delete
+                            </Link>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+  

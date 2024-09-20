9be077fcc6dafb5dd2102eda7fb76fe437a2a255
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-09-21 01:26:36 +0530

    Chore: add styling and settings button

--- app/components/sites/SiteCard.tsx
+++ app/components/sites/SiteCard.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { BookA } from "lucide-react";
+import { BookA, Settings2Icon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import DefaultImage from "@/public/defaultImage.png";
@@ -27,9 +27,16 @@ export default function SiteCard({ site }: SiteCardProps) {
         width={450}
         height={300}
       />
-      <CardHeader>
-        <CardTitle>{site.name}/{site.subdirectory}</CardTitle>
-        <CardDescription>{site.description}</CardDescription>
+      <CardHeader className="flex flex-row p-2 items-center justify-evenly w-full mt-4 mb-4">
+        <div className="flex flex-col w-[200px]">
+        <CardTitle className="truncate">{site.name}</CardTitle>
+        <CardDescription className="line-clamp-2">{site.description}</CardDescription>
+        </div>
+        <div className="flex w-full items-start justify-end">
+          <Button variant="outline" className="bg-muted hover:bg-black/30" asChild>
+            <Link href={`/dashboard/sites/${site.id}/settings`}><Settings2Icon className="size-4"/></Link>
+          </Button>
+        </div>
       </CardHeader>
       <CardFooter>
         <Button asChild className="w-full">

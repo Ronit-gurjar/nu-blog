b112f125d695cfc883547a1ddcebfa53304f7e30
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-09-24 03:27:46 +0530

    Create: action to handle permissions for subscription type

--- app/actions.ts
+++ app/actions.ts
@@ -12,6 +12,25 @@ export async function CreateSiteAction(prevState: any, formData : FormData) {
    
     const user = await useRequireUser();
 
+    const [subStatus, sites] = await Promise.all([
+      prisma.subscription.findUnique({
+        where: {
+          userId: user.id,
+        },
+        select: {
+          status: true,
+        },
+      }),
+      prisma.site.findMany({
+        where: {
+          userId: user.id,
+        },
+      }),
+    ]);
+  
+    if (!subStatus || subStatus.status !== "active") {
+      if (sites.length < 1) {
+        // Allow creating a site
         const submission = await parseWithZod(formData, {
           schema: SiteCreationSchema({
             async isSubdirectoryUnique() {
@@ -25,6 +44,40 @@ export async function CreateSiteAction(prevState: any, formData : FormData) {
           }),
           async: true,
         });
+  
+        if (submission.status !== "success") {
+          return submission.reply();
+        }
+  
+        const response = await prisma.site.create({
+          data: {
+            description: submission.value.description,
+            name: submission.value.name,
+            subdirectory: submission.value.subdirectory,
+            userId: user.id,
+          },
+        });
+  
+        return redirect("/dashboard/sites");
+      } else {
+        // user alredy has one site dont allow
+        return redirect("/dashboard/pricing");
+      }
+    } else if (subStatus.status === "active") {
+      // User has a active plan he can create sites...
+      const submission = await parseWithZod(formData, {
+        schema: SiteCreationSchema({
+          async isSubdirectoryUnique() {
+            const exisitngSubDirectory = await prisma.site.findUnique({
+              where: {
+                subdirectory: formData.get("subdirectory") as string,
+              },
+            });
+            return !exisitngSubDirectory;
+          },
+        }),
+        async: true,
+      });
   
       if (submission.status !== "success") {
         return submission.reply();
@@ -40,6 +93,7 @@ export async function CreateSiteAction(prevState: any, formData : FormData) {
       });
       return redirect("/dashboard/sites");
     }
+  }
   
 export async function CreatePostAction(prevState: any, formData: FormData) {
     const user = await useRequireUser();
